af19951ee7edf1576a656723c57f903ce6dd1cce
pscamposs <pscampos221@gmail.com>
2024-07-02 17:39:30 -0300

    feat: image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  typescript: { 
+  typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
